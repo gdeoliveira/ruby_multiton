8ed4a248a2baac8112167e268027d50bfe187ae4
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-11-22 22:25:53 -0300

    Add a couple of specs for Multiton.

--- spec/unit/multiton_spec.rb
+++ spec/unit/multiton_spec.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
+require "support/shared_examples/a_multiton.rb"
 require "multiton"
 
 describe Multiton do
+  # rubocop:disable Style/EmptyLiteral
+  [123, :symbol, Array.new, Hash.new, Module.new, Object.new, Set.new, String.new].each do |object|
+    # rubocop:enable Style/EmptyLiteral
+    it "raises an exception when extending an object of type `#{object.class}`" do
+      expect { object.extend(described_class) }.to raise_error(TypeError)
+    end
+  end
+
+  it "does not raise an exception when extending an object of type `Class`" do
+    expect { Class.new.extend(described_class) }.not_to raise_error
+  end
+
+  describe "class" do
+    let(:klass) do
+      Class.new do
+        extend Multiton
+      end
+    end
+
+    it_behaves_like "a multiton"
+  end
 end
