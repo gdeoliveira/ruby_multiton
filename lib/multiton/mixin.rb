584f81c5f6f13f09320edcbaeb2fb8789c7f540c
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-11-27 16:53:34 -0300

    Make MultitonMixin#_dump method public so it will be called on older Ruby versions.

--- lib/multiton/mixin.rb
+++ lib/multiton/mixin.rb
@@ -2,18 +2,16 @@ module Multiton
   ##
   # TODO: Add documentation.
   module Mixin
+    def _dump(_level)
+      self.class.instance_variable_get(:@__multiton_instances).key(self)
+    end
+
     def clone
       raise TypeError, "can't clone instance of multiton `#{self.class.name}`"
     end
 
     def dup
       raise TypeError, "can't dup instance of multiton `#{self.class.name}`"
-    end
-
-    private
-
-    def _dump(_level)
-      self.class.instance_variable_get(:@__multiton_instances).key(self)
     end
   end
 end
