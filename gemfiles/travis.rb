95a0b8710d3c7d3c782e69109e1798b8be8e6d37
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2016-11-27 04:11:26 -0300

    Add basic Travis configuration.

---
+++ gemfiles/travis.rb
@@ -0,0 +1,7 @@
+source "https://rubygems.org".freeze
+
+gem "codeclimate-test-reporter".freeze
+gem "json".freeze
+gem "rake".freeze
+gem "rspec".freeze
+gem "simplecov".freeze
---
+++ gemfiles/travis_1.8.7.rb
@@ -0,0 +1,8 @@
+source "https://rubygems.org".freeze
+
+gem "rspec".freeze
+gem "simplecov".freeze
+
+# 1.8.7 specific versions
+gem "json".freeze, "~> 1.8.3".freeze
+gem "rake".freeze, "~> 10.5.0".freeze
