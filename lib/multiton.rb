d4155b916be977772a67de1127a9634f450bf604
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2017-04-04 19:58:22 -0300

    Add documentation to `Multiton` module.

--- lib/multiton.rb
+++ lib/multiton.rb
@@ -4,7 +4,66 @@ require "multiton/mixin".freeze
 require "multiton/version".freeze
 
 ##
-# TODO: Add documentation.
+# The Multiton extension implements the {multiton pattern}[https://en.wikipedia.org/wiki/Multiton_pattern] in a manner
+# similar to Ruby standard library's Singleton[https://ruby-doc.org/stdlib-2.4.1/libdoc/singleton/rdoc/Singleton.html]
+# module. As a matter of fact Multiton can be used to implement the singleton pattern in a very straightforward way:
+#
+#   class C
+#     extend Multiton
+#   end
+#
+#   C.instance.object_id  #=> 47362978769640
+#   C.instance.object_id  #=> 47362978769640
+#
+# In order to generate and access different instances a +key+ must be provided as a parameter to the +initialize+
+# method:
+#
+#   class C
+#     extend Multiton
+#
+#     def initialize(key)
+#       # Initialize this instance (if needed).
+#     end
+#   end
+#
+#   one = C.instance(:one)
+#   two = C.instance(:two)
+#
+#   one.object_id               #=> 46941338337020
+#   C.instance(:one).object_id  #=> 46941338337020
+#
+#   two.object_id               #=> 46941338047140
+#   C.instance(:two).object_id  #=> 46941338047140
+#
+# Almost any object or set of objects will work as a valid +key+. The +key+ assembly is transparently handled by
+# Multiton internally, leaving the end user with an uncluttered and familiar way of designing their classes:
+#
+#   class Person
+#     extend Multiton
+#
+#     attr_reader :full_name
+#
+#     def initialize(name:, surname:)
+#       self.full_name = "#{surname}, #{name} --- (id: #{object_id})".freeze
+#     end
+#
+#     private
+#
+#     attr_writer :full_name
+#   end
+#
+#   alice = Person.instance(name: "Alice", surname: "Alcorta")
+#   bob = Person.instance(name: "Bob", surname: "Berman")
+#
+#   alice.full_name                                               #=> "Alcorta, Alice --- (id: 46921440327980)"
+#   Person.instance(name: "Alice", surname: "Alcorta").full_name  #=> "Alcorta, Alice --- (id: 46921440327980)"
+#
+#   bob.full_name                                                 #=> "Berman, Bob --- (id: 46921440022260)"
+#   Person.instance(name: "Bob", surname: "Berman").full_name     #=> "Berman, Bob --- (id: 46921440022260)"
+#
+# Note that even though keyword arguments will work with Multiton, passing the keywords in a different order will
+# generate a different instance. It is left up to the end user to design their +key+ in a way that minimizes the
+# possibility of improper use.
 module Multiton
   extend Extensible
 
--- lib/multiton/version.rb
+++ lib/multiton/version.rb
@@ -1,5 +1,5 @@
 module Multiton
   ##
-  # Current version of Multiton
+  # Current version of Multiton.
   VERSION = "0.0.1.pre".freeze
 end
