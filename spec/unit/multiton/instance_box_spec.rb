c50f3d54e42bf4bda819b5fb3b9f9cfb7aa56511
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2017-04-16 15:45:15 -0300

    Refactor InstanceBox specs.

--- spec/unit/multiton/instance_box_spec.rb
+++ spec/unit/multiton/instance_box_spec.rb
@@ -9,19 +9,40 @@ describe Multiton::InstanceBox do
   let(:key) { Marshal.dump instance }
   let(:sync) { instance_box.instance_variable_get(:@sync) }
 
+  def expect_method_to_be_called_within_shared_lock(original, *args)
+    expect_to_be_within_shared_lock
+    return_value = original.call(*args)
+    expect_to_be_within_shared_lock
+    return_value
+  end
+
+  def expect_exclusive_lock_to_be_on_current_thread
+    expect(sync.sync_ex_locker).to be(Thread.current)
+  end
+
+  def expect_shared_lock_to_be_on_current_thread
+    expect(sync.sync_sh_locker.keys.first).to be(Thread.current)
+  end
+
+  def expect_to_be_within_exclusive_lock
+    expect_exclusive_lock_to_be_on_current_thread
+    expect(sync).to be_locked
+    expect(sync).to be_exclusive
+  end
+
+  def expect_to_be_within_shared_lock
+    expect_shared_lock_to_be_on_current_thread
+    expect(sync).to be_locked
+    expect(sync).to be_shared
+  end
+
   before(:each) do
     expect(hash).to receive(:[]=).once.with(key, instance).and_wrap_original do |original, *args|
-      expect(sync.sync_ex_locker).to be(Thread.current)
-      expect(sync).to be_locked
-      expect(sync).to be_exclusive
+      expect_to_be_within_exclusive_lock
       expect(hash).to be_empty
-
       original.call(*args)
-
       expect(hash).not_to be_empty
-      expect(sync).to be_exclusive
-      expect(sync).to be_locked
-      expect(sync.sync_ex_locker).to be(Thread.current)
+      expect_to_be_within_exclusive_lock
     end
 
     expect(sync).not_to be_locked
@@ -33,17 +54,7 @@ describe Multiton::InstanceBox do
   describe "#get" do
     it "returns instance given a key" do
       expect(hash).to receive(:[]).once.with(key).and_wrap_original do |original, *args|
-        expect(sync.sync_sh_locker.keys.first).to be(Thread.current)
-        expect(sync).to be_locked
-        expect(sync).to be_shared
-
-        return_value = original.call(*args)
-
-        expect(sync).to be_shared
-        expect(sync).to be_locked
-        expect(sync.sync_sh_locker.keys.first).to be(Thread.current)
-
-        return_value
+        expect_method_to_be_called_within_shared_lock(original, *args)
       end
 
       expect(instance_box.get(key)).to be(instance)
@@ -53,17 +64,7 @@ describe Multiton::InstanceBox do
   describe "#key" do
     it "returns key given an instance" do
       expect(Multiton::Utils).to receive(:hash_key).once.with(hash, instance).and_wrap_original do |original, *args|
-        expect(sync.sync_sh_locker.keys.first).to be(Thread.current)
-        expect(sync).to be_locked
-        expect(sync).to be_shared
-
-        return_value = original.call(*args)
-
-        expect(sync).to be_shared
-        expect(sync).to be_locked
-        expect(sync.sync_sh_locker.keys.first).to be(Thread.current)
-
-        return_value
+        expect_method_to_be_called_within_shared_lock(original, *args)
       end
 
       expect(instance_box.key(instance)).to eq(key)
