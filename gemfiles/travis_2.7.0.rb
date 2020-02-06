4b9bdaa33b4b44caac12119cdd91a8bf53c45806
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2020-02-06 19:03:22 -0300

    Fix Travis gemfiles.

---
+++ gemfiles/travis_2.7.0.rb
@@ -0,0 +1,10 @@
+source "https://rubygems.org".freeze
+
+gem "codeclimate-test-reporter".freeze
+gem "extensible".freeze
+gem "rake".freeze
+gem "rspec".freeze
+gem "simplecov".freeze
+
+# 2.7.0 specific versions
+gem "sync".freeze
