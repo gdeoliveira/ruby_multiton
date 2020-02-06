1d5d326163fe9288506cd23bac2baf2ba7cf3563
Gabriel de Oliveira <deoliveira.gab@gmail.com>
2020-02-06 18:53:15 -0300

    Fix missing travis gemfile and tweak jruby versions.

---
+++ gemfiles/travis_1.8.7.rb
@@ -0,0 +1,9 @@
+source "https://rubygems.org".freeze
+
+gem "extensible".freeze
+gem "rspec".freeze
+gem "simplecov".freeze
+
+# 1.8.7 specific versions
+gem "json".freeze, "~> 1.8.3".freeze
+gem "rake".freeze, "~> 10.5.0".freeze
